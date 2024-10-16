7668ef43cd53ec64af09ce392ad9fa431e4e6bcf
Leandro dos Santos <leandrosebold16@gmail.com>
2024-10-16 13:00:12 -0300

    refactor: implements token validation function inside of jwt package

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/leandro-d-santos/no-code-api/internal/utils"
 	internalJwt "github.com/leandro-d-santos/no-code-api/pkg/jwt"
 )
@@ -26,31 +25,14 @@ func AuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		token, err := getToken(headerToken)
-		if err != nil {
+		claims, valid := internalJwt.ValidateToken(headerToken)
+		if !valid {
 			utils.ResJson(c, http.StatusUnauthorized, false, "Token inválido", nil)
 			c.Abort()
 			return
 		}
 
-		claims, ok := token.Claims.(*internalJwt.Claims)
-		if !ok || !token.Valid {
-			utils.ResJson(c, http.StatusUnauthorized, false, "Token inválido", nil)
-			c.Abort()
-			return
-		}
 		c.Set("userId", claims.UserId)
 		c.Next()
 	}
-}
-
-func getToken(headerToken string) (*jwt.Token, error) {
-	jwtSecret := internalJwt.GetJWTSecret()
-	keyFunc := func(token *jwt.Token) (interface{}, error) {
-		return jwtSecret, nil
-	}
-	return jwt.ParseWithClaims(
-		headerToken,
-		&internalJwt.Claims{},
-		keyFunc)
 }
--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -5,12 +5,18 @@ import (
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/leandro-d-santos/no-code-api/config"
+	"github.com/leandro-d-santos/no-code-api/internal/logger"
 )
 
 type Claims struct {
 	UserId uint `json:"userId"`
 	jwt.RegisteredClaims
 }
+
+var (
+	issuer   string = "https://node-code-api/api"
+	audience string = "https://node-code-api"
+)
 
 func GetJWTSecret() []byte {
 	return []byte(config.Env.JWTSecret)
@@ -24,7 +30,8 @@ func GenerateJWT(userId uint) (string, error) {
 		RegisteredClaims: jwt.RegisteredClaims{
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(jwtDuration)),
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
-			Issuer:    "no-code-api",
+			Issuer:    issuer,
+			Audience:  jwt.ClaimStrings{audience},
 		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
@@ -33,4 +40,25 @@ func GenerateJWT(userId uint) (string, error) {
 		return "", err
 	}
 	return tokenString, nil
+}
+
+func ValidateToken(userToken string) (*Claims, bool) {
+	jwtSecret := GetJWTSecret()
+	logger := logger.NewLogger("ValidateToken")
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	}
+
+	token, err := jwt.ParseWithClaims(userToken, &Claims{}, keyFunc)
+	if err != nil {
+		logger.InfoF("Error to parse claims: %v", err.Error())
+		return nil, false
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, false
+	}
+
+	return claims, true
 }
